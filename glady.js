90f311a9a1264e28f41129c5066fbc8fd093e863
WebFrontender <91338170+WebFrontender@users.noreply.github.com>
2022-04-22 07:44:22 +0300

    Update glady.js

--- glady.js
+++ glady.js
@@ -1,9 +1,3 @@
-var isDesktop = (function() {
-  return !('ontouchstart' in window) || !('onmsgesturechange' in window);
-})();
-window.isDesktop = isDesktop;
-if( isDesktop ){
-  
 var VanillaTilt = (function () {
 'use strict';
 
@@ -545,5 +539,3 @@ VanillaTilt.init(document.querySelectorAll(".contacts"), {
     glare: true,
     "max-glare":1,
   });
-  
-}
